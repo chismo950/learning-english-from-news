61c52979f17141c080b7f27a6f362f7bddf24d87
Eason <craigchismo950@gmail.com>
2025-04-17 17:39:26 +1200

    change api method

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(_req: NextRequest) {
+export async function GET(_req: NextRequest) {
   return NextResponse.json({
     domainSuffixWhitelist: [
       'donate.stripe.com',
